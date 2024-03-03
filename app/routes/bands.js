f01817a3a3412a77b8e34209039a19c1f1041a36
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2024-03-03 17:58:56 +0000

    nested song routes into band routes

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -3,13 +3,49 @@ import EmberObject from "@ember/object";
 
 let Band = EmberObject.extend({
   name: "",
+
+  slug: computed("name", function () {
+    return this.get("name").dasherize();
+  }),
+});
+
+let Song = EmberObject.extend({
+  title: "",
+  rating: 0,
+  band: "",
 });
 
 export default Route.extend({
   model: function () {
-    let ledZeppelin = Band.create({ name: "Led Zeppelin" });
-    let pearlJam = Band.create({ name: "Pearl Jam" });
-    let fooFighters = Band.create({ name: "Foo Fighters" });
+    let blackDog = Song.create({
+      title: "Black Dog",
+      band: "Led Zeppelin",
+      rating: 3,
+    });
+
+    let yellowLedbetter = Song.create({
+      title: "Yellow Ledbetter",
+      band: "Pearl Jam",
+      rating: 4,
+    });
+
+    let pretender = Song.create({
+      title: "The Pretender",
+      band: "Foo Fighters",
+      rating: 2,
+    });
+    let daughter = Song.create({
+      title: "Daughter",
+      band: "Pearl Jam",
+      rating: 5,
+    });
+
+    let ledZeppelin = Band.create({ name: "Led Zeppelin", songs: [blackDog] });
+    let pearlJam = Band.create({
+      name: "Pearl Jam",
+      songs: [yellowLedbetter, daughter],
+    });
+    let fooFighters = Band.create({ name: "Foofighters", songs: [pretender] });
 
     return [ledZeppelin, pearlJam, fooFighters];
   },
